efe21adef52046a343513151ca54aa1b4479cf07
devopscomputer <pauloti.engenharia@gmail.com>
2026-05-13 23:55:33 -0300

    chore: update src/mev/simulation/state_simulator.rs

--- src/mev/simulation/state_simulator.rs
+++ src/mev/simulation/state_simulator.rs
@@ -1,9 +1,11 @@
 use crate::mev::amm::uniswap_v2::V2PoolState;
+use crate::mev::amm::uniswap_v3::V3PoolState;
 use ethers::types::{Address, U256};
 
 #[derive(Debug, Clone)]
 pub enum AmmState {
     UniswapV2(V2PoolState),
+    UniswapV3(V3PoolState),
 }
 
 #[derive(Debug, Clone)]
@@ -26,6 +28,13 @@ impl StateSimulator {
                 let (next, result) = pool.apply_swap_exact_in(token_in, token_out, amount_in)?;
                 Some(PostSwapSimulation {
                     state_after: AmmState::UniswapV2(next),
+                    slippage_impact_bps: result.price_impact_bps,
+                })
+            }
+            AmmState::UniswapV3(pool) => {
+                let (next, result) = pool.simulate_exact_in(token_in, token_out, amount_in)?;
+                Some(PostSwapSimulation {
+                    state_after: AmmState::UniswapV3(next),
                     slippage_impact_bps: result.price_impact_bps,
                 })
             }
